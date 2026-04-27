c5c9f82fe66044a7e8a9991d847b70e5f4d51416
ohayo <9443476+ohayo@users.noreply.github.com>
2026-04-28 08:27:25 +1000

    fix: move limits from config to spacebar policies

--- server/api/spacebar-compat/policies.ts
+++ server/api/spacebar-compat/policies.ts
@@ -20,23 +20,23 @@ router.get('/instance/domains', (req: Request, res: Response) => {
 
 router.get('/instance/config', (_req: Request, res: Response) => {
   const instanceConfig: SpacebarInstanceConfig = {
-    limits_user_maxGuilds: 99999999999,
+    limits_user_maxGuilds: config.limits.guilds_per_account.max ?? 0,
     limits_user_maxBio: 99999999999,
-    limits_guild_maxEmojis: 99999999999,
-    limits_guild_maxRoles: 99999999999,
-    limits_message_maxCharacters: 99999999999,
-    limits_message_maxAttachmentSize: 99999999999,
-    limits_message_maxEmbedDownloadSize: 99999999999,
+    limits_guild_maxEmojis: config.limits.emojis_per_guild.max ?? 0,
+    limits_guild_maxRoles: config.limits.roles_per_guild.max ?? 0,
+    limits_message_maxCharacters: config.limits.messages.max ?? 0,
+    limits_message_maxAttachmentSize: config.limits.attachments.max_size ?? 0,
+    limits_message_maxEmbedDownloadSize: config.limits.embeds.max ?? 0,
     limits_channel_maxWebhooks: 99999999999,
     register_dateOfBirth_required: false,
     register_password_required: true,
-    register_disabled: false,
+    register_disabled: config.instance.flags.includes("NO_REGISTRATION"),
     register_requireInvite: false,
-    register_allowNewRegistration: true,
+    register_allowNewRegistration: !config.instance.flags.includes("NO_REGISTRATION"),
     register_allowMultipleAccounts: true,
     guild_autoJoin_canLeave: true,
     guild_autoJoin_guilds_x: config.instance.flags.flatMap(x => x.toLowerCase().startsWith("autojoin:") ? [x.toLowerCase().replace("autojoin:", "")] : []),
-    register_email_required: true,
+    register_email_required: config.instance.flags.includes("VERIFIED_EMAIL_REQUIRED"),
     can_recover_account: false, //Uhh depends really
   };
   
